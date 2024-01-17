658336d35a68476fb2f680698d208d765574b840
hadisAhmadi <hadiseh.ahmadi.aghdam@gmail.com>
2024-01-18 00:14:03 +0330

    refactor User

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,20 +1,20 @@
 package org.example.entity;
 
-public class user {
+public class User {
     private int id;
     private String name;
     private String username;
     private String email;
     private String password;
 
-    public user(String name, String username, String email, String password) {
+    public User(String name, String username, String email, String password) {
         this.name = name;
         this.username = username;
         this.email = email;
         this.password = password;
     }
 
-    public user() {
+    public User() {
     }
 
     @Override
