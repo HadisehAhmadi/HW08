df6bb6a20fcdc170f5f32a11f0781cdb6f2ce256
hadisAhmadi <hadiseh.ahmadi.aghdam@gmail.com>
2024-01-17 22:02:31 +0330

    create category.java in entity package

---
+++ src/main/java/org/example/entity/category.java
@@ -0,0 +1,48 @@
+package org.example.entity;
+
+public class category {
+    private int id;
+    private String name;
+    private String description;
+
+    public category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
